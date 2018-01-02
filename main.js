bd5d3b9e9ed3cde30830dbbd91f5e2e25ef9340b
ZhaoZhiYing <31733268+ZhaoZhiYing@users.noreply.github.com>
2018-01-02 19:31:38 +0800

    rewrite

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
 let n
 初始化()
 setInterval(()=>{
-    makeLeave($(`#images > img:nth-child(${x(n)})`)) //ES6 ``
-        .one('transitionend', function(x){
-            makeEnter($(x.currentTarget))
-        })
-    makeCurrent(getImage(n+1))
-    n += 1
+    makeLeave(getImage(n))
+     .one('transitionend', (e)=>{
+        makeEnter($(e.currentTarget))
+     })
+     makeCurrent($(`#images>img:nth-child(${x(n+1)})`))
+     n += 1
 },3000)
+ 
+//封装函数
+function getImage(n){
+    return $(`#images>img:nth-child(${x(n)})`)
+}
 
 function x(n){
-    if(n>3){ 
+    if(n > 3){
         n = n%3
         if (n === 0){
-            n = 3 
+            n = 3
         }
-    } // n 的值永远是 1 2 3
+    }
     return n
 }
 
 function 初始化(){
     n = 1
-    $(`#images > img:nth-child(${n})`).addClass('current')
-        .siblings().addClass('enter')
+    $(`#images>img:nth-child(${n})`).addClass('current')
+     .siblings().addClass('enter')
 }
 
 function makeCurrent($node){
-    $node.removeClass('enter leave').addClass('current')
+    $node.removeClass('leave enter').addClass('current')
 }
 function makeLeave($node){
     $node.removeClass('current enter').addClass('leave')
@@ -34,8 +39,4 @@ function makeLeave($node){
 }
 function makeEnter($node){
     $node.removeClass('current leave').addClass('enter')
-}
-
-function getImage(n){
-    return $(`#images > img:nth-child(${x(n)})`)
 }
