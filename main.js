a839bc81e47758717e30512954cc73d6fca43fb9
ZhaoZhiYing <31733268+ZhaoZhiYing@users.noreply.github.com>
2018-03-07 16:29:52 +0800

    add buttons controls

--- main.js
+++ main.js
@@ -1,59 +1,73 @@
-let n
-初始化()
-let timer = setInterval(()=>{
-    makeLeave(getImage(n))
-     .one('transitionend', (e)=>{
-        makeEnter($(e.currentTarget))
-     })
-     makeCurrent($(`#images>img:nth-child(${x(n+1)})`))
-     n += 1
-},3000)
- 
-document.addEventListener('visibilitychange', function(e){
-    console.log(document.hidden)
-    if(document.hidden){
-        window.clearInterval(timer)
-    }else{
-        timer = setInterval(()=>{
-            makeLeave(getImage(n))
-             .one('transitionend', (e)=>{
-                makeEnter($(e.currentTarget))
-             })
-             makeCurrent($(`#images>img:nth-child(${x(n+1)})`))
-             n += 1
-        },3000)
-    }
+let $buttons = $('#buttons > button')
+let $images = $('#images')
+let $img = $images.children('img')
+let current = 0
+
+makeFakeSlides()
+$images.css({transform:'translateX(-300px)'})
+bindEvents()
+$(next).on('click', function(){
+  goToSlide(current+1)
+})
+$(previous).on('click', function(){
+  goToSlide(current-1)
 })
 
+let timer = setInterval(function(){
+  goToSlide(current+1)
+},2000)
+$('.container').on('mouseenter', function(){
+  window.clearInterval(timer)
+}).on('mouseleave', function(){
+  timer = setInterval(function(){
+    goToSlide(current+1)
+  },2000)
+})
 
-//封装函数
-function getImage(n){
-    return $(`#images>img:nth-child(${x(n)})`)
-}
-
-function x(n){
-    if(n > 3){
-        n = n%3
-        if (n === 0){
-            n = 3
-        }
-    }
-    return n
-}
-
-function 初始化(){
-    n = 1
-    $(`#images>img:nth-child(${n})`).addClass('current')
-     .siblings().addClass('enter')
+function bindEvents(){
+  $('#buttons ').on('click', 'button', function(e){
+    let $button = $(e.currentTarget) 
+    let index = $button.index()
+    goToSlide(index)
+  })
 }
 
-function makeCurrent($node){
-    $node.removeClass('leave enter').addClass('current')
+//previous next 
+function goToSlide(index){
+  if(index > $buttons.length-1){
+    index = 0
+  }else if(index <0){
+    index = $buttons.length - 1
+  }
+  console.log('current', 'index')
+  console.log(current, index)
+  if(current === $buttons.length -1 && index === 0){
+    // 最后一张到第一张
+    console.log('here')
+    $images.css({transform:`translateX(${-($buttons.length + 1) * 300}px)`})
+      .one('transitionend', function(){
+        $images.hide()
+        $images.offset() // .offset() 可以触发 re-layout
+        $images.css({transform:`translateX(${-(index+1)*300}px)`}).show()
+      })
+
+  }else if(current === 0 && index === $buttons.length - 1){
+    // 第一张到最后一张
+    $images.css({transform:`translateX(0px)`})
+      .one('transitionend', function(){
+        $images.hide().offset()
+        $images.css({transform:`translateX(${-(index+1)*300}px)`}).show()
+      })
+
+  }else{
+    $images.css({transform:`translateX(${- (index+1) * 300}px)`})
+  }
+  current = index
 }
-function makeLeave($node){
-    $node.removeClass('current enter').addClass('leave')
-    return $node //*
-}
-function makeEnter($node){
-    $node.removeClass('current leave').addClass('enter')
+
+function makeFakeSlides(){
+    let $firstCopy = $img.eq(0).clone(true)
+    let $lastCopy = $img.eq($img.length-1).clone(true)
+    $images.append($firstCopy)
+    $images.prepend($lastCopy)
 }
